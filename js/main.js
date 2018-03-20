a61d71b6182be75fcdc7afbb70b410230c438e42
Victor Romanov <romanovvictor1985@gmail.com>
2018-03-20 12:04:27 +0200

    header + media requests added

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ $(function(){
       dots:true
   });
 });
+
+$(function(){
+	$(".toggleMobMenu").on("click",function(){
+		$(".menuCont").stop().slideToggle();
+	});
+
+	$(".searchIcon").on("click",function(){
+		$(".header__form").stop().slideToggle();
+	});
+
+});
